dfb2a9ded7ed06c6522ae2e8a3e12eb4c0b01ba3
MamedYahyayev <yahyayev2001@gmail.com>
2022-11-21 23:15:55 +0400

    Add -v, -h flags

--- keyword_finder.sh
+++ keyword_finder.sh
@@ -8,6 +8,11 @@ CYAN=$(tput setaf 6)
 PURPLE=$(tput setaf 5)
 NORMAL=$(tput sgr0)
 
+# Version
+KF_VERSION='1.0.1'
+
+# Docs
+DOC_URL='https://github.com/MamedYahyayev/keyword-finder'
 
 SUPPORTED_FILE_FORMATS=("docx" "pdf")
 
@@ -16,82 +21,104 @@ keywords=()
 txt_files=()
 declare -A file_map
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DOCX_TO_TXT_CONVERTER_PATH="$SCRIPT_DIR/docx2txt-1.4/docx2txt.sh"
-
-function build_find_command() {
-    local len=${#SUPPORTED_FILE_FORMATS[@]}
-    command="find . -type f \(";
-    for i in "${!SUPPORTED_FILE_FORMATS[@]}"; do 
-        command="${command} -iname \*.${SUPPORTED_FILE_FORMATS[$i]}"
-        local result=`expr $len - 1`
-        if [[ $i -ne result ]];
-        then
-            command="${command} -o"
-        fi
-    done
-
-    command="${command} \) -printf '%f\n'";
-}
 
 # Helper Log Functions
-function error() { 
-    echo $RED"Error: $1"$NORMAL 
+function error() {
+    echo $RED"Error: $1"$NORMAL
 }
 
-function success() { 
+function success() {
     echo $GREEN"$1"$NORMAL
 }
 
 function info() {
-    echo $CYAN"$1"$NORMAL 
+    echo $CYAN"$1"$NORMAL
 }
 
-function warning() { 
+function warning() {
     echo $YELLOW"$1"$NORMAL
 }
 
-function debug() { 
-    echo $PURPLE"==> $1"$NORMAL 
+function debug() {
+    echo $PURPLE"==> $1"$NORMAL
 }
 
 # Helper str functions
 function is_str_empty() {
-    if [[ -z "${1// }" ]]; then
+    if [[ -z "${1// /}" ]]; then
         true
     else
         false
     fi
 }
 
+while getopts 'vhd:' OPTION; do
+    case "$OPTION" in
+    v)
+        echo "keyword finder $KF_VERSION"
+        ;;
+    h)
+        echo "For documentation refer to: $DOC_URL"
+        ;;
+    d)
+        dvalue="$OPTARG"
+        if is_str_empty $dvalue; then
+            error "Please specify directory path where you want to search your files!"
+            exit 1
+        else
+            directory_path=$dvalue
+        fi
 
-directory_path=$1
-if is_str_empty $directory_path; then
- error "Please specify directory path where you want to search your files!"
- exit;
-fi
+        # TODO: Check the given directory exist, if not print error message
+        ;;
+
+    f) 
+        fvalue="$OPTARG"
+        echo "Give file path to convert and search on it"
+    ?)
+        echo "Unknown flag, plese type $YELLOW sh keyword-finder.sh -h$NORMAL for more info" >&2
+        exit 1
+        ;;
+    esac
+done
+shift "$(($OPTIND - 1))"
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+DOCX_TO_TXT_CONVERTER_PATH="$SCRIPT_DIR/docx2txt-1.4/docx2txt.sh"
+
+function build_find_command() {
+    local len=${#SUPPORTED_FILE_FORMATS[@]}
+    command="find . -type f \("
+    for i in "${!SUPPORTED_FILE_FORMATS[@]}"; do
+        command="${command} -iname \*.${SUPPORTED_FILE_FORMATS[$i]}"
+        local result=$(expr $len - 1)
+        if [[ $i -ne result ]]; then
+            command="${command} -o"
+        fi
+    done
+
+    command="${command} \) -printf '%f\n'"
+}
 
 cd "$directory_path"
 
 txt_exports_dir_path="$directory_path/__txt_exports__"
 if [[ ! -d $txt_exports_dir_path ]]; then
-    eval $(mkdir '__txt_exports__')        
+    eval $(mkdir '__txt_exports__')
 fi
 
 build_find_command
 
-function collect_given_directory_files() {    
+function collect_given_directory_files() {
     OIFS="$IFS"
     IFS=$'\n'
-    for file in `eval $command`
-    do
+    for file in $(eval $command); do
         files+=($file)
     done
     IFS="$OIFS"
 }
 
 collect_given_directory_files
-
 
 function convert_docx_to_txt() {
     file_path="$directory_path/$file"
@@ -107,7 +134,7 @@ function convert_pdf_to_txt() {
     export_path="$txt_exports_dir_path/$txt_file"
 
     cd "$SCRIPT_DIR/pdf2text"
-    ./pdf2text $file_path > $txt_file_path
+    ./pdf2text $file_path >$txt_file_path
 }
 
 function convert_to_txt() {
@@ -122,10 +149,10 @@ function convert_to_txt() {
     elif [[ $file =~ $pdf_regex ]]; then
         convert_pdf_to_txt $file
     fi
-        
+
     txt_file_path="$directory_path/$txt_file"
     export_path="$txt_exports_dir_path/$txt_file"
-    
+
     if [[ -d $txt_exports_dir_path ]]; then
         eval $(mv "$txt_file_path" "$export_path")
     fi
@@ -155,30 +182,30 @@ function convert() {
         fi
 
         exported_file_path="$txt_exports_dir_path/$txt_file"
-        
+
         if [[ -e $exported_file_path ]]; then
             read -p $YELLOW"$file already converted, do you want to override: Type y if you want to override, otherwise press enter:$NORMAL $GREEN" need_override
 
             if [[ $need_override == 'y' ]]; then
                 convert_to_txt $file
-                converted_files=`expr $converted_files + 1`
+                converted_files=$(expr $converted_files + 1)
             else
-                skipped_files=`expr $skipped_files + 1`
+                skipped_files=$(expr $skipped_files + 1)
             fi
-            
+
         else
             convert_to_txt $file
-            converted_files=`expr $converted_files + 1`
+            converted_files=$(expr $converted_files + 1)
         fi
 
-        if [[ `expr $converted_files % 5` -eq 0 && $converted_files -ne 0 ]]; then
+        if [[ $(expr $converted_files % 5) -eq 0 && $converted_files -ne 0 ]]; then
             info "$converted_files files already converted..."
         fi
 
         txt_files+=($exported_file_path)
         file_map+=([$exported_file_path]=$file_path)
     done
-    
+
     echo $NORMAL$YELLOW"Total: $skipped_files files skipped"$NORMAL
     success "Total: $converted_files files converted"
     IFS="$OIFS"
@@ -195,15 +222,15 @@ echo $'\n***************************************************************\n'
 
 read -p "Enter your keywords and separate them with comma: $YELLOW" str_keywords
 
-IFS=',' read -r -a keywords_arr <<< "$str_keywords"
+IFS=',' read -r -a keywords_arr <<<"$str_keywords"
 
-for i in "${!keywords_arr[@]}"; do 
+for i in "${!keywords_arr[@]}"; do
     keyword="${keywords_arr[$i]}"
     trimmed_keyword="${keyword//' '/''}"
     keywords+=($trimmed_keyword)
 done
 
-for key in "${keywords[@]}"; do 
+for key in "${keywords[@]}"; do
     echo $NORMAL"Keyword $YELLOW'$key'$NORMAL found in the following files:"
     for file in "${!file_map[@]}"; do
         if grep -w -q -i $key "${file}"; then
@@ -215,8 +242,8 @@ done
 
 # TODO List
 # 1. add some extra options
-    # -v print version of the project
-    # -h get help
-    # --ignore_override don't generate the file again and again 
-    # --override override all the files that are generated before
-    # -f give the files by seperating them with comma, to convert and search 
+# -v print version of the project
+# -h get help
+# --ignore_override don't generate the file again and again
+# --override override all the files that are generated before
+# -f give the files by seperating them with comma, to convert and search
